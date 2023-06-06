8c598f13f37a216aab7d10e9197038621d6e077e
rm86973 <raquel.quel1411@gmail.com>
2023-06-06 12:01:28 -0300

    Balao

---
+++ Balonismo/src/Balao.java
@@ -0,0 +1,12 @@
+public class Balao {
+
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    double altura;
+    double peso;
+    String porte;
+    int capacidadePessoas;
+
+}
